773f4d22f786415e6f5097e17692e092c4a7b20c
Iuliia Shikhanova <yshihanova@gmail.com>
2023-02-16 16:52:48 +0000

    products

---
+++ online-shop/src/components/ProductList/ProductItem.jsx
@@ -0,0 +1,15 @@
+import "./productList.css";
+
+export const ProductItem = ({ item }) => {
+  const { url, name, price } = item;
+  return (
+    <div>
+      <img src={url} alt="" />
+      <div className="product-info-container">
+        <p>{name}</p>
+        <p>{price}</p>
+      </div>
+      <Button text="Buy me!" value={name} size="sm" color="secondary" />
+    </div>
+  );
+};
---
+++ online-shop/src/data/products.js
@@ -0,0 +1,50 @@
+export const products = [
+    {
+      name: "HYDRATING FACE CREAM",
+      price: 75.99,
+      url: "https://cdn.shopify.com/s/files/1/0498/8764/1749/products/cream_600x.jpg?v=1624901800",
+      id: 1,
+    },
+    {
+      name: "REGENERATING FACE SERUM",
+      price: 135.49,
+      url: "https://cdn.shopify.com/s/files/1/0498/8764/1749/products/rosehip2_600x.png?v=1617089619",
+      id: 2,
+    },
+    {
+      name: "BALANCING TONER",
+      price: 35.49,
+      url: "https://cdn.shopify.com/s/files/1/0498/8764/1749/products/tonercolor_600x.jpg?v=1624901753",
+      id: 3,
+    },
+    {
+      name: "BRIGHTENING FACE CLEANSER",
+      price: 28.49,
+      url: "https://cdn.shopify.com/s/files/1/0498/8764/1749/products/Facewash_600x.jpg?v=1624960848",
+      id: 4,
+    },
+    {
+      name: "NOURISHING ELIXIR",
+      price: 35.49,
+      url: "https://cdn.shopify.com/s/files/1/0498/8764/1749/products/apricot2_600x.png?v=1612390326",
+      id: 5,
+    },
+    {
+      name: "HEALING CONCENTRATE",
+      price: 40.49,
+      url: "https://cdn.shopify.com/s/files/1/0498/8764/1749/products/moringa_600x.png?v=1624903416",
+      id: 6,
+    },
+    {
+      name: "HYDRATING FACE CREAM NIGTH",
+      price: 50.49,
+      url: "https://cdn.shopify.com/s/files/1/0498/8764/1749/products/cream_600x.jpg?v=1624901800",
+      id: 7,
+    },
+    {
+      name: "ELIXIR",
+      price: 99.99,
+      url: "https://cdn.shopify.com/s/files/1/0498/8764/1749/products/Moringa_600x.jpg?v=1624903432",
+      id: 8,
+    },
+  ];
