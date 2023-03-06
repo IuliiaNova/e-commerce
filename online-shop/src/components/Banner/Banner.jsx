d18097757d354fc56b7637ab99a5fa0a449910bd
Iuliia Shikhanova <yshihanova@gmail.com>
2023-03-06 17:45:08 +0000

    banner style

--- online-shop/src/components/Banner/Banner.jsx
+++ online-shop/src/components/Banner/Banner.jsx
@@ -7,9 +7,9 @@ import { flower } from "../image/grafic.png";
 export const Banner = () => {
     return (
         <section className="">
-                    <div className="presentation">
+                    <div className="presentation flex gap-8">
                         <img src={flower} />
-                        <Link to="/shop" className="nav-item"><button className="button-banner">KNOW MORE</button></Link>
+                        <Link to="/shop" className="nav-item mt-8"><button className="button-banner mt-8">KNOW MORE</button></Link>
                         
                     </div>
         </section>
