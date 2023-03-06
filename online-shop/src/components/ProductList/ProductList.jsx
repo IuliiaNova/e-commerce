d0d31cc52e62be20cecbc40b0af9ce26b718da53
Iuliia Shikhanova <yshihanova@gmail.com>
2023-03-06 12:57:50 +0000

    productlist{use memo}: This would prevent unnecessary re-renders of the ProductItem components if the products state hasn't changed

--- online-shop/src/components/ProductList/ProductList.jsx
+++ online-shop/src/components/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect } from 'react';
+import React, {useState, useEffect, useMemo } from 'react';
 import { ProductItem } from "./ProductItem";
 import "./products.css";
 
@@ -9,26 +9,36 @@ export const ProductsList = () => {
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch('http://localhost:3000/products');
-      const data = await response.json();
-      setProducts(data);
+      try {
+        const response = await fetch('http://localhost:3000/products');
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error(error);
+      }
     };
 
     fetchProducts();
   }, []);
 
+// Here we prevent unnecessary re-renders of the ProductItem components if the products state hasn't changed
+
+  const memoizedProducts = useMemo(() => {
+    return products.map((product, idx) => {
+      return <ProductItem 
+      key={`beauty-item-${idx}`} 
+      item={product} 
+      id={product.id} 
+      url={product.url} 
+      title={product.title} 
+      price={product.price} 
+      count={product.count} />;
+    });
+  }, [products]);
+
   return (
     <div className="products-list">
-      {products.map((product, idx) => {
-        return <ProductItem 
-        key={`beauty-item-${idx}`} 
-        item={product} 
-        id={product.id} 
-        url={product.url} 
-        title={product.title} 
-        price={product.price} 
-        count={product.count} />;
-      })}
+      {memoizedProducts}
     </div>
   );
 };
